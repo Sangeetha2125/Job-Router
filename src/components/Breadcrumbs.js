79c4473c32877c92c4afe08c69f16cdb4d965e79
Sangeetha2125 <gsangeetha7113@gmail.com>
2023-06-08 15:56:38 +0530

    BreadCrumbs Added

---
+++ src/components/Breadcrumbs.js
@@ -0,0 +1,28 @@
+import { useLocation} from "react-router-dom"
+import { Link } from "react-router-dom"
+
+function Breadcrumbs(){
+
+    const location = useLocation()
+    let currentlink = ''
+
+    const crumbs = location.pathname.split('/').filter( (crumb) => crumb !== '').map(
+        (crumb)=>{
+            currentlink+=`/${crumb}`
+            return <div className="crumb">
+                <Link to={currentlink} key={crumb}>{crumb}</Link>
+            </div>
+        }
+    )
+        
+    return <div className="breadcrumbs">
+        <div className="crumbs">
+            <div className="crumb">
+                <Link to="/">home</Link>
+            </div>
+            {crumbs}
+        </div>
+    </div>
+}
+
+export default Breadcrumbs
--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../pages/Navbar"
 import Footer from "../pages/Footer"
+import Breadcrumbs from "../components/Breadcrumbs"
 
 function RootLayout(){
     return  <div className="root-layout">
       <header>
         <Navbar />
       </header>
+      <Breadcrumbs />
       <main>
         <Outlet />
       </main>
