b70627ce87c0dd09d11ea7252de4f4a868d777d7
Sangeetha2125 <gsangeetha7113@gmail.com>
2023-06-11 15:33:27 +0530

    Career Page Designed

--- src/pages/Careerlist.js
+++ src/pages/Careerlist.js
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom"
+import person from '../images/person.png'
 
 function Careerlist(props){
     const careers = props.careers
     return <div>
         {careers.map((career)=>(
-            <div key={career.id}>
-                <Link to={career.id.toString()}>
-                    <p>{career.title}</p>
-                    <p>Based on {career.location}</p>
+            <div className="career-wrap" key={career.id}>
+                <Link to={career.id.toString()} className="career-container">
+                    <img src={person} alt="" />
+                    <div>
+                        <p>{career.title}</p>
+                        <p>Based on {career.location}</p>
+                    </div>
                 </Link>
                 <button className="delete-career">Delete</button>
             </div>
